6556a6a20dab7fa37b44231a260601cdd70a7d0d
Shaik Dadapeer <shaikdadapeer362@gmail.com>
2021-07-07 10:13:09 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-color">
+    <h1 className="heading-1">Super Over League</h1>
+    <div className="img-styling">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="img-width"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="img-width"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
